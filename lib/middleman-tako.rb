af97ad5ddf0de285c8fe548fb2cb97b7e89b89bd
Dunya Kirkali <dunyakirkali@gmail.com>
2014-01-18 19:14:00 +0100

    Added templates

--- lib/middleman-tako.rb
+++ lib/middleman-tako.rb
@@ -4,3 +4,5 @@ require 'middleman-tako/version'
 require 'middleman-tako/template'
 
 Middleman::Templates.register :slim, Middleman::Tako::Template
+
+Slim::Engine.set_default_options :pretty => true
