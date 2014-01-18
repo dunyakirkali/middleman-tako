c718a89e549f0b2da4f23b70687dbbde21958296
Dunya Kirkali <dunyakirkali@gmail.com>
2014-01-18 16:52:24 +0100

    improving

---
+++ lib/middleman-tako.rb
@@ -0,0 +1,6 @@
+require 'middleman-core'
+
+require 'middleman-tako/version'
+require 'middleman-tako/template'
+
+Middleman::Templates.register :slim, Middleman::Tako::Template
---
+++ lib/middleman-tako/version.rb
@@ -0,0 +1,5 @@
+module Middleman
+  module Tako
+    VERSION = "0.1.0"
+  end
+end
